5200a0c8d1226fbeb8ef72766ec20d98a3c5ae8a
mnaofficialbd <mna.official92@gmail.com>
2026-01-28 12:47:04 +0600

    Mount Handler added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 const app: Application = express();
 
@@ -11,6 +13,8 @@ app.use(
 );
 
 app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
   res.send("Running the MNA_MediStore_Server");
