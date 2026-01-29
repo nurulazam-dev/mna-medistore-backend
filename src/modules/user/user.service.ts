e13756564fe0e611d535ac904eb918017c0ba883
mnaofficialbd <mna.official92@gmail.com>
2026-01-30 02:11:52 +0600

    user service create

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,31 @@
+import { User } from "../../../generated/prisma/client";
+import { prisma } from "../../lib/prisma";
+
+const getAllUser = async () => {
+  const result = await prisma.user.findMany();
+  return result;
+};
+
+const updateUserStatus = async (id: string, data: Partial<User>) => {
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  const result = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data,
+  });
+  return result;
+};
+
+export const userService = {
+  getAllUser,
+  updateUserStatus,
+};
