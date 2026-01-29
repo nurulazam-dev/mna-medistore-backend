9d43e4cba7bf7b1581d1d5173d5725a9b2498971
mnaofficialbd <mna.official92@gmail.com>
2026-01-29 13:16:41 +0600

    medicine router create

---
+++ src/modules/medicine/medicine.router.ts
@@ -0,0 +1,13 @@
+import express, { Router } from "express";
+import { MedicineController } from "./medicine.controller";
+import auth, { UserRole } from "../../middleware/auth";
+
+const router = express.Router();
+
+// router.get("/", MedicineController.getAllMedicine);
+// router.get("/:id", MedicineController.getMedicineById);
+// router.patch("/:id", auth(UserRole.ADMIN), MedicineController.updateMedicine);
+// router.delete("/:id", auth(UserRole.ADMIN), MedicineController.deleteMedicine);
+router.post("/", auth(UserRole.SELLER), MedicineController.createMedicine);
+
+export const medicineRouter: Router = router;
