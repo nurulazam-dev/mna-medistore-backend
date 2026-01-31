b6a8c43970ee7f558f4bdcc126daf9cad942ae1e
mnaofficialbd <mna.official92@gmail.com>
2026-01-31 14:50:08 +0600

    add-review service updated

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,67 +1,37 @@
+import { OrderStatus } from "../../../generated/prisma/enums";
 import { prisma } from "../../lib/prisma";
 
 const createReview = async (userId: string, payload: any) => {
-  const { items, shipping_address, total_amount } = payload;
+  const { medicineId, rating, comment } = payload;
 
-  return await prisma.$transaction(async (tx) => {
-    const order = await tx.order.create({
-      data: {
-        customerId: userId,
-        status: OrderStatus.PLACED,
-        payment_method: PaymentMethod.CASH_ON_DELIVERY,
-        total_amount: Number(total_amount),
-        shipping_address,
+  const hasOrdered = await prisma.order.findFirst({
+    where: {
+      customerId: userId,
+      status: OrderStatus.DELIVERED,
+      items: {
+        some: {
+          medicineId,
+        },
       },
-    });
-
-    const orderItemsData = [];
-
-    for (const item of items) {
-      const medicine = await tx.medicine.findUnique({
-        where: {
-          id: item.medicineId,
-        },
-      });
-
-      if (!medicine) {
-        throw new Error("Medicine not found!");
-      }
-
-      const unitPrice = Number(medicine.price);
-      const quantity = Number(item.quantity);
-      const availableStock = Number(medicine.stock || 0);
-
-      if (quantity > availableStock) {
-        throw new Error(`You can't order more than available stock!`);
-      }
-
-      orderItemsData.push({
-        orderId: order.id,
-        medicineId: item.medicineId,
-        sellerId: item.sellerId,
-        quantity: quantity,
-        unit_price: unitPrice,
-        sub_total: unitPrice * quantity,
-      });
-
-      await tx.medicine.update({
-        where: {
-          id: item.medicineId,
-        },
-        data: {
-          stock: {
-            decrement: quantity,
-          },
-        },
-      });
-    }
-
-    await tx.orderItem.createMany({
-      data: orderItemsData,
-    });
-
-    return order;
+    },
   });
+
+  if (!hasOrdered) {
+    throw new Error(
+      "You can only review medicine that you have successfully purchased!",
+    );
+  }
+
+  const result = await prisma.review.create({
+    data: {
+      medicineId,
+      customerId: userId,
+      rating: Number(rating),
+      comment,
+    },
+  });
+
+  return result;
 };
 
 export const reviewService = {
